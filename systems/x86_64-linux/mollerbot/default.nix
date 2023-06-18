594e8cf9eaaa5f354759d3e1f74bcbc394388616
Jack W <jack-git@cetera.uk>
2023-06-18 17:58:14 -0400

    fix: :ambulance: whoops turns out that doesn't work yet

--- systems/x86_64-linux/mollerbot/default.nix
+++ systems/x86_64-linux/mollerbot/default.nix
@@ -18,7 +18,7 @@
       use-xdg-base-directories = false; # Some bug makes $PATH not update to the new directories so for now I'm disabling this
       auto-optimise-store = true;
     };
-    registry.nixpkgs.flake = "${inputs.nixpkgs}"; # https://github.com/NixOS/nix/pull/6530, "incompatible changes"
+    registry.nixpkgs.flake = inputs.nixpkgs; # https://github.com/NixOS/nix/pull/6530, "incompatible changes"
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
