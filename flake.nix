fc2cfe5a86e6ffd037559a561ed44d841cedac5a
Jack W. <git@jack.cab>
2023-10-04 16:24:34 -0400

    flake.lock: Update

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     snowfall-lib = {
-      url = "github:snowfallorg/lib/dev";
+      url = "github:snowfallorg/lib";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-software-center = {
