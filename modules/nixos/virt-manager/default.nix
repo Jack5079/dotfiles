07c7a99c90699b67741ef25b40419f0c34ca8a63
Jack W. <git@jack.cab>
2023-11-05 22:15:15 -0500

    fix: :coffin: programs.virt-manager.enable is now built-in
    https://github.com/NixOS/nixpkgs/commit/ac30ac808a1425ebc1b7bd513b6e851f1311c249

--- modules/nixos/virt-manager/default.nix
+++ modules/nixos/virt-manager/default.nix
@@ -4,10 +4,6 @@ let
   cfg = config.programs.virt-manager;
 in
 {
-  options.programs.virt-manager = {
-    enable = lib.mkEnableOption "virt-manager";
-  };
-
   config = lib.mkIf cfg.enable
     {
       virtualisation.libvirtd.enable = true;
