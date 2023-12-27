4b180012f5140dab2a4e102be6e10e14526ea438
Jack W. <git@jack.cab>
2023-12-26 19:38:43 -0500

    feat: :bento: NO MORE FUCKING MINECRAFT FONT!!!!
    NOW I DON'T NEED TO LOOK LIKE I WATCH FUCKING ANIME AND "MAKE MEMES" WHENEVER I SCREENSHARE

---
+++ lib/default.nix
@@ -0,0 +1,33 @@
+{ lib }: {
+  patchOutput = originalPackage: cmd: originalPackage.overrideAttrs (old: {
+    pname = "${old.pname}-overridden";
+
+    # Using `buildCommand` replaces the original packages build phases.
+    buildCommand = ''
+      set -euo pipefail
+
+      ${
+        # Copy original files, for each split-output (`out`, `dev` etc.).
+        # E.g. `${package.dev}` to `$dev`, and so on. If none, just "out".
+        # Symlink all files from the original package to here (`cp -rs`),
+        # to save disk space.
+        # We could alternatiively also copy (`cp -a --no-preserve=mode`).
+        lib.concatStringsSep "\n"
+          (map
+            (outputName:
+              ''
+                echo "Copying output ${outputName}"
+                set -x
+                cp -rs --no-preserve=mode "${originalPackage.${outputName}}" "''$${outputName}"
+                set +x
+              ''
+            )
+            (old.outputs or ["out"])
+          )
+      }
+
+      ${cmd}
+    '';
+
+  });
+}
