37e8802fdddd15bd009de58983bfbf34201d7719
Jack W. <git@jack.cab>
2024-01-20 17:44:29 -0500

    fix: :alien: vesktop changed names

--- packages/vesktop-with-sane-icon/default.nix
+++ packages/vesktop-with-sane-icon/default.nix
@@ -1,5 +1,5 @@
 { lib, pkgs }: lib.me.patchOutput pkgs.vesktop ''
   rm -r $out/share/icons
   mkdir -p $out/share/icons/hicolor/scalable/apps/
-  cp ${./vencorddesktop.svg} $out/share/icons/hicolor/scalable/apps/vencorddesktop.svg
+  cp ${./vesktop.svg} $out/share/icons/hicolor/scalable/apps/vesktop.svg
 ''
