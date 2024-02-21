db525931b1527c93f780d3754768e062d9a18864
Jack W. <git@jack.cab>
2024-02-20 22:25:17 -0500

    feat: :sparkles: jetbrains mono

--- systems/x86_64-linux/mollerbot/desktop.nix
+++ systems/x86_64-linux/mollerbot/desktop.nix
@@ -1,14 +1,13 @@
 { pkgs, config, ... }: {
-  # xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ]; # https://github.com/NixOS/nixpkgs/issues/155291#issuecomment-1166199585
   services.xserver = {
     enable = true;
     displayManager.gdm.enable = true;
     desktopManager = {
       gnome.enable = true;
-      plasma5.enable = false;
     };
     excludePackages = [ pkgs.xterm ];
   };
+
   environment.gnome.excludePackages = with pkgs; [
     gnome-tour # I've used GNOME before
     gnome-photos
@@ -18,7 +17,7 @@
     gnome.geary
     snapshot # I do not have a webcam
     epiphany # Sometimes I uncomment this to test WebKit
-    gnome.gnome-music # I use Amberol instead
+    gnome.gnome-music # I use G4Music instead
   ];
   environment.sessionVariables.NIXOS_OZONE_WL = "1"; # https://gitlab.freedesktop.org/xorg/xserver/-/issues/1317
   # Enable sound with pipewire.
@@ -30,16 +29,10 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   fonts = {
-    packages = [ pkgs.inter pkgs.noto-fonts-cjk-sans pkgs.source-sans-pro pkgs.go-font pkgs.monaspace ];
+    packages = [ pkgs.inter pkgs.noto-fonts-cjk-sans pkgs.source-sans-pro pkgs.go-font pkgs.monaspace pkgs.jetbrains-mono ];
     fontDir.enable = true;
     fontconfig.defaultFonts = {
       sansSerif = [ "Inter Variable" "Inter Variable Regular" "Inter" "Inter Regular" "Cantarell" "DejaVu Sans" ];
