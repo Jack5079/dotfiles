edb91c652c414c4c4b0a6587697b5f73ef3947ec
Jack W. <git@jack.cab>
2024-07-15 16:48:51 -0400

    perf: Free up 250+ GB by removing old home-manager profiles

--- homes/x86_64-linux/jack/default.nix
+++ homes/x86_64-linux/jack/default.nix
@@ -94,4 +94,31 @@
       "--data=${config.home.homeDirectory}/Documents"
     ];
   };
+
+  # https://github.com/nix-community/home-manager/issues/4672
+  systemd.user.services.expire-hm-profiles = {
+    Unit.Description = "Clean up HM user profiles.";
+
+    Service = {
+      Type = "oneshot";
+      ExecStart = lib.getExe (pkgs.writeShellApplication {
+        name = "expire-hm-profiles-start-script";
+        text = ''
+          ${lib.getExe config.nix.package} profile wipe-history \
+            --profile "${config.xdg.stateHome}/nix/profiles/home-manager" \
+            --older-than '7d'
+        '';
+      });
+    };
+  };
+  systemd.user.timers.expire-hm-profiles = {
+    Unit.Description = "Clean up HM user profiles.";
+
+    Timer = {
+      OnCalendar = "weekly";
+      Persistent = true;
+    };
+
+    Install.WantedBy = [ "timers.target" ];
+  };
 }
