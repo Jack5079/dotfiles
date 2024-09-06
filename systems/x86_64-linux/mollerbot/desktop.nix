1436286ecb8f1edfd673ce411aeb4b5e8118e4e1
Jack W. <git@jack.cab>
2024-09-06 18:13:36 -0400

    Revert "refactor: No more /usr/share/font bind needed since march"
    This reverts commit aae36bd7c127138bd142dbac2f4d778409ccece0.

--- systems/x86_64-linux/mollerbot/desktop.nix
+++ systems/x86_64-linux/mollerbot/desktop.nix
@@ -40,7 +40,27 @@
     fontconfig.defaultFonts = {
       sansSerif = [ "Inter Variable" "Inter Variable Regular" "Inter" "Inter Regular" "Cantarell" "DejaVu Sans" "Noto Color Emoji" ];
       monospace = [ "Commit Mono" "DejaVu Sans Mono" ];
-      emoji = [ "Noto Color Emoji" ]; # I love NixOS defaults 😀
+      emoji = [ "Noto Color Emoji" ];
     };
   };
+  # Custom fonts in Flatpak applications <https://github.com/NixOS/nixpkgs/issues/119433#issuecomment-1326957279>
+  system.fsPackages = [ pkgs.bindfs ];
+  fileSystems =
+    let
+      mkRoSymBind = path: {
+        device = path;
+        fsType = "fuse.bindfs";
+        options = [ "ro" "resolve-symlinks" "x-gvfs-hide" ];
+      };
+      aggregatedFonts = pkgs.buildEnv {
+        name = "system-fonts";
+        paths = config.fonts.packages;
+        pathsToLink = [ "/share/fonts" ];
+      };
+    in
+    {
+      # Create an FHS mount to support flatpak host icons/fonts
+      "/usr/share/icons" = mkRoSymBind (config.system.path + "/share/icons");
+      "/usr/share/fonts" = mkRoSymBind (aggregatedFonts + "/share/fonts");
+    };
 }
