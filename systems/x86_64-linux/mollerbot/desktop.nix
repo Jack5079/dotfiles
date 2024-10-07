8ebdcda94987263ecfdc084b249b2075e79815cf
Jack W. <git@jack.cab>
2024-10-07 18:28:58 -0400

    refactor: Uninstall rygel

--- systems/x86_64-linux/mollerbot/desktop.nix
+++ systems/x86_64-linux/mollerbot/desktop.nix
@@ -21,6 +21,7 @@
     gnome-contacts # Don't think I've ever used this
   ];
   services.gnome.gnome-remote-desktop.enable = false; # I don't use it, and it pulls in freerdp (725 MB)
+  services.gnome.rygel.enable = false; # "Rygel UPnP Mediaserver", 897.7 MiB
   environment.sessionVariables.NIXOS_OZONE_WL = "1"; # https://gitlab.freedesktop.org/xorg/xserver/-/issues/1317
   environment.sessionVariables.NIXPKGS_ALLOW_UNFREE = "1";
 
