967aeb4f9a3ecec9e2fe3911f722d8cbb2940a66
Jack W. <git@jack.cab>
2025-01-23 21:06:22 -0500

    refactor: remove hardened profile

--- modules/nixos/hardened/default.nix
+++ modules/nixos/hardened/default.nix
@@ -2,15 +2,17 @@
 
 {
   imports = [
-    "${modulesPath}/profiles/hardened.nix"
+    # "${modulesPath}/profiles/hardened.nix"
   ];
 
   config = {
-    # Needed for flatpak
-    security.unprivilegedUsernsClone = lib.mkForce true;
-    security.allowUserNamespaces = lib.mkForce true;
-    security.virtualisation.flushL1DataCache = null; # Use kernel default. Hardened uses always: "flushes L1 data cache every time the hypervisor enters the guest.  May incur significant performance cost."
-    security.allowSimultaneousMultithreading = true; # "Disabling SMT means that only physical CPU cores will be usable at runtime, potentially at significant performance cost."
+    # # Firefox won't open
+    # environment.memoryAllocator.provider = lib.mkForce "libc";
+    # # Needed for flatpak
+    # security.unprivilegedUsernsClone = lib.mkForce true;
+    # security.allowUserNamespaces = lib.mkForce true;
+    # security.virtualisation.flushL1DataCache = null; # Use kernel default. Hardened uses always: "flushes L1 data cache every time the hypervisor enters the guest.  May incur significant performance cost."
+    # security.allowSimultaneousMultithreading = true; # "Disabling SMT means that only physical CPU cores will be usable at runtime, potentially at significant performance cost."
     # https://xeiaso.net/blog/paranoid-nixos-2021-07-18
     security.sudo.execWheelOnly = true;
     environment.defaultPackages = lib.mkForce [ ]; # Currently only perl, rsync, and strace
